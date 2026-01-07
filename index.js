bba44ac87863cc49c516e73d95d64d3ceb1421e2
ELYORLEY <luis.vass21@gmail.com>
2026-01-07 02:09:39 -0300

    index.js

--- index.js
+++ index.js
@@ -134,7 +134,6 @@ app.get("/reset-queue", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("API running on port", PORT);
+  console.log(`API running on port ${PORT}`);
 });
-
 
